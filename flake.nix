8c0822da20be191e19614dcbb603a1441d9b2a89
Kerstin Humm <kerstin@erictapen.name>
2021-12-28 18:12:34 +0100

    init nix flake

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "A very basic flake";
+
+  outputs = { self, nixpkgs }: let
+    pkgs = import nixpkgs { system = "x86_64-linux"; };
+  in {
+    devShell.x86_64-linux = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        elmPackages.elm
+      ];
+    };
+
+  };
+}
